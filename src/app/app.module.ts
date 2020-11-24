7e5816e0a6104abba7bae8cc8cd9270c1f3533a9
Andrew Kern <andrew.kern47@yahoo.com>
2020-11-24 18:23:48 -0500

    Initial Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { ContactComponent } from './contact/contact.component';
+import { TrendsComponent } from './trends/trends.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SettingsComponent } from './settings/settings.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavigationComponent,
+    ContactComponent,
+    TrendsComponent,
+    ProfileComponent,
+    SettingsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
